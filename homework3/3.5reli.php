70ad6fd82c75f9d0e22618079b9694f19799155e
yuxian11 <84006134+yuxian11@users.noreply.github.com>
2021-11-01 21:42:24 +0800

    Add files via upload

---
+++ homework3/3.5reli.php
@@ -0,0 +1,48 @@
+<?php
+/*
+ * Created on 2016-7-20
+ */
+SimCalendar('2021-10');//显示8月份日历
+function SimCalendar($date)
+{
+  /**
+   * 简单日历输出,本函数需要cal_days_in_month的支持
+   * @param $date Y-m 要输出的日期
+   */
+  echo '<table border="1">
+  <thead>
+    <tr>
+      <th>日</th>
+      <th>一</th>
+      <th>二</th>
+      <th>三</th>
+      <th>四</th>
+      <th>五</th>
+      <th>六</th>
+    </tr>
+  </thead>
+  <tbody>';
+    $date_array = explode('-', $date);
+    $start_week = 0;//从星期天开始为0
+    $month = cal_days_in_month(CAL_GREGORIAN, $date_array[1], $date_array[0]);//当月的天数
+    $wstar = date('w', strtotime($date . '-01'));//当月从星期几天始
+    $rows = ceil(($wstar + $month) / 7);//总行数
+    $mday = 1;//第几天
+    for ($i = 0; $i < $rows; $i++) {
+      echo '<tr>';
+      for ($d = 0; $d < 7; $d++) {
+        $nowday = 7 * $i + $d + $start_week;
+        if ($nowday >= $wstar && $mday <= $month) {
+          $temp = date('d', strtotime($date . '-' . $mday));
+          echo '<td>'.$temp . '</td>';
+          $mday++;
+        } else {
+          echo '<td>&nbsp;</td>';
+        }
+      }
+      echo '</tr>';
+    }
+  echo '</tbody>
+</table>';
+}
+?>
